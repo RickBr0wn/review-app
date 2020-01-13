dfed1eb1e4e221dc7421d31f07e205d83cee113e
Rick Brown <ricky.brown.00@gmail.com>
2020-01-13 17:31:21 +0000

    adds react navigation with 2 screens

--- src/screens/home.js
+++ src/screens/home.js
@@ -1,11 +1,15 @@
 import React from 'react'
-import {View, Text, StyleSheet} from 'react-native'
+import {View, Text, StyleSheet, Button} from 'react-native'
 import {globalStyles as gs} from '../styles/global'
 
-const Home = () => {
+const Home = ({navigation}) => {
   return (
     <View style={gs.container}>
       <Text>Home</Text>
+      <Button
+        title="go to review details"
+        onPress={() => navigation.navigate('ReviewDetails')}
+      />
     </View>
   )
 }
