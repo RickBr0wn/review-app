b7eb9b1e32c47779c97dbbe12d8342ee5f284c15
Rick Brown <ricky.brown.00@gmail.com>
2020-01-13 16:38:23 +0000

    added screens

---
+++ src/screens/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+import {globalStyles as gs} from '../styles/global'
+
+const About = () => {
+  return (
+    <View style={gs.container}>
+      <Text>About</Text>
+    </View>
+  )
+}
+
+export default About
---
+++ src/screens/reviewDetails.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import {View, Text, StyleSheet} from 'react-native'
+import {globalStyles as gs} from '../styles/global'
+
+const ReviewDetails = () => {
+  return (
+    <View style={gs.container}>
+      <Text>Review Details</Text>
+    </View>
+  )
+}
+
+export default ReviewDetails
